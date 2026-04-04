0fd0c495f5cb03ee18c0f2e414fb01c030378ea1
ozancili-del <ozan.cili@gmail.com>
2026-04-03 19:44:37 -0500

    Update pricelabs-proxy.js

--- pages/api/pricelabs-proxy.js
+++ pages/api/pricelabs-proxy.js
@@ -42,9 +42,10 @@ async function fetchORBookings(sinceDate) {
 }
 
 export default async function handler(req, res) {
-  if (req.method === 'OPTIONS') {
-    return res.status(200).end();
-  }
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  if (req.method === 'OPTIONS') return res.status(200).end();
 
   const { action } = req.query;
 

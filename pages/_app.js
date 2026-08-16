a0e83a50ebf8b49a19a598bd2b5fa5ac25293958
LoserGod <ozan.cili@gmail.com>
2026-08-16 15:08:42 -0500

    Load Destiny chat bubble site-wide

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,19 @@
 import Script from "next/script";
+import { useRouter } from "next/router";
+
+const CHAT_EXCLUDED_ROUTES = ["/concierge", "/destin-ai-concierge", "/ozan", "/app", "/guestview", "/tv"];
+
+function shouldLoadChat(pathname) {
+  return !CHAT_EXCLUDED_ROUTES.some((route) => pathname === route || pathname.startsWith(`${route}/`));
+}
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
+  const pathname = router.asPath?.split("?")[0]?.split("#")[0] || router.pathname || "/";
   return (
     <>
       <Component {...pageProps} />
+      {shouldLoadChat(pathname) ? <Script src="/destiny-head.js" strategy="lazyOnload" /> : null}
       <Script
         id="destin-gtm"
         strategy="afterInteractive"

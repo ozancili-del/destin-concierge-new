162b6bdc26a28dec061828c8fa6b0e63c9a1b64a
LoserGod <ozan.cili@gmail.com>
2026-08-24 17:19:32 -0500

    Add branded site favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import Script from "next/script";
 import { useRouter } from "next/router";
 import "../public/dcg-core.js";
@@ -14,6 +15,13 @@ export default function App({ Component, pageProps }) {
   const pathname = router.asPath?.split("?")[0]?.split("#")[0] || router.pathname || "/";
   return (
     <>
+      <Head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/favicon-48.png" type="image/png" sizes="48x48" />
+        <link rel="icon" href="/favicon-192.png" type="image/png" sizes="192x192" />
+        <link rel="apple-touch-icon" href="/favicon-192.png" />
+        <link rel="manifest" href="/manifest.json" />
+      </Head>
       <Component {...pageProps} />
       {shouldLoadChat(pathname) ? <Script src="/destiny-head.js" strategy="lazyOnload" /> : null}
     </>
